76abf7daa104ba264604b53de8e7a0218aab46c2
Kai Pahl <kai@kaipahl.de>
2016-05-08 16:53:28 +0200

    Refactor comments template for webmention

--- src/_themes/aas-evolution2013/comments.php
+++ src/_themes/aas-evolution2013/comments.php
@@ -3,108 +3,135 @@
  * Comment template file.
  *
  * @package aas-evolution2013
- * @since aas-evolution2013 1.0
+ * @since aas-evolution2013 1.1
  */
- 
+
  // Do not delete these lines
 if (!empty($_SERVER['SCRIPT_FILENAME']) && 'comments.php' == basename($_SERVER['SCRIPT_FILENAME']))
 	die ('Please do not load this page directly. Thanks!');
 
 	/* This variable is for alternating comment background */
 	$oddcomment = 'graybox';
-	
 	$commStatus = 'closed';
 ?>
 
 <!-- You can start editing here. -->
-<div id="comments">
-	
-	<div class="comments-description clearfix">
-				
+<div class="o-box-comments">
+
 	<?php if ('open' == $commStatus) : ?>
-	
-		<h3><?php comments_number('<big>Keine</big> Kommentare','<big>1</big> Kommentar','<big>%</big> Kommentare' );?></h3>
-	
+	<div class="m-comments-description">
+
+
+		<h3><?php comments_number('<strong>Keine</strong> Kommentare','<strong>1</strong> Kommentar','<strong>%</strong> Kommentare' );?></h3>
+
 		<?php if (('open' == $commStatus) && ('open' == $post->ping_status)) {  ?>
 			<p>Sie können <a href="#respond">einen <strong>Kommentar</strong> hinterlassen</a>, oder <a href="<?php trackback_url(display); ?>">einen <strong>Trackback</strong> setzen</a>.</p>
-			
-			
+
+
 		<?php } elseif (!('open' == $commStatus) && ('open' == $post->ping_status)) { ?>
 			<p>Die Kommentare sind geschlossen, aber Sie können <a href="<?php trackback_url(display); ?> ">einen Trackback setzen</a>.</p>
-			
-			
+
+
 		<?php } elseif (!('open' == $commStatus) && !('open' == $post->ping_status)) { ?>
-			<p>Kommentare und Pings sind geschlossen.	</p>		
+			<p>Kommentare und Pings sind geschlossen.	</p>
 		<?php } ?>
-		
+
 		<p>Alle Kommentare in diesem Blog können per <strong><a href="<?php bloginfo_rss('comments_rss2_url'); ?>">RSS 2.0</a>-Feed</strong> abonniert werden.</p>
-		
+
+	</div><!-- /.m-comments-description -->
 	<?php endif; ?>
-</div><!-- /#comments -->
-	
+
+
+
 
 <!-- ============================================== -->
 
+	<div class="m-comments-description-nocomments">
+		<div class="wrapper">
+			<h4 class="a-comments-description-title">Wo kann man Kommentare eingeben?</h4>
+			<p>Nach elf Jahren habe ich die Kommentare im Blog mangels Zeit für Kommentarverwaltung geschlossen. Es kann noch kommentiert werden. Es ist aber etwas umständlicher geworden.</p>
+			<ol>
+				<li>Das Kommentarblog <strong><a href="http://allesausseraas.de/">http://allesausseraas.de/</a></strong>, aufgezogen von den Lesern Sternburg und OtherTimes</li>
+				<li>Sogenannte „<strong>Webmentions</strong>“ mit einem eigenen Blog. Siehe <a href="https://indiewebcamp.com/Webmention#Andy_Sylvester_with_WordPress_Webmention_plugin">IndieWebCamp</a></li>
+			</ol>
+		</div>
+	</div><!-- /.m-comments-description-nocomments -->
 
-<?php if ($comments) : ?>
-	
-	<?php
-		$comment_anzahl = count($comments);
-		$comment_counter = 0;
-		$comment_class = 'first';
-	?>
 
-		<ol id="commentlist">
+	<?php if ($comments) : ?>
+
+		<?php
+			$comment_anzahl = count($comments);
+			$comment_counter = 0;
+			$comment_class = 'first';
+		?>
+
+		<ol class="m-comments">
 		<?php foreach ($comments as $comment) : ?>
-			<?php 
+			<?php
 				$comment_counter++;
 				if ($comment_counter == $comment_anzahl) {
 					$comment_class = 'last';
 				} else if ($comment_counter == 2) {
 					$comment_class = '';
 				}
+
+				$comment_id = get_comment_ID();
+				$comment_type = get_comment_type($comment_id);
+				if ($comment_type === 'comments') {
+					$comment_type_str = 'Kommentare';
+				} else if ($comment_type === 'trackback') {
+					$comment_type_str = 'Trackback';
+				} else if ($comment_type === 'pingback') {
+					$comment_type_str = 'Pingback';
+				} else {
+					$comment_type_str = '';
+				}
+
+				$comment_type_markup = '<a href="#comment-' . $comment_id . '" title="">';
+				$comment_type_markup .= get_comment_type($comment_id);
+				$comment_type_markup .= $comment_type_str;
+				$comment_type_markup .= '</a>';
+
+				$comment_date_markup = '<a href="#comment-' . $comment_id . '" title="">';
+				$comment_date_markup .= get_comment_time('D, j M Y, G:i');
+				$comment_date_markup .= '</a>';
+
+				$comment_author_markup = get_comment_author_link() . ' ';
+				$comment_edit_markup = '<a href="' . get_edit_comment_link('Edit') . '" class="a-btn-edit">Edit</a>';
 			?>
-		
-			<li class="comment <?php echo comment_author() ?>" id="comment-<?php comment_ID() ?>">
-				<?php 
-					if ($comment_class == 'first') {	echo '<a name="comment-first"></a>'; } 
-					if ($comment_class == 'last') {	echo '<a name="comment-last"></a>'; } 
+
+			<li class="m-comment <?php comment_author() ?>" id="comment-<?php echo $comment_id; ?>">
+				<?php
+					if ($comment_class == 'first') { echo '<a name="comment-first"></a>'; }
+					if ($comment_class == 'last') {	echo '<a name="comment-last"></a>'; }
 				?>
-				<ul class="labels comment-meta clearfix">
-					<li class="comment-date">
-						<a href="#comment-<?php comment_ID() ?>" title=""><?php comment_type('Kommentar', 'Trackback', 'Pingback'); ?></a>
-						<a href="#comment-<?php comment_ID() ?>" title=""><?php comment_time('D, j M Y, G:i') ?></a>
+				<ul class="m-comment-meta">
+					<li class="m-comment-date">
+						<?php echo $comment_type_markup; ?>
+						<?php echo $comment_date_markup; ?>
 					</li>
-					<li class="comment-author">
-						<span><?php comment_author_link() ?> <?php edit_comment_link('Edit'); ?></span>
+					<li class="m-comment-author">
+						<?php echo $comment_author_markup; ?>
+						<?php echo $comment_edit_markup; ?>
 					</li>
-				</ul>
-				
-				<div class="comment-text">
+				</ul><!-- /.m-comment-meta -->
+
+				<div class="m-comment-text">
 					<?php comment_text() ?>
 				</div>
-				
-			</li>
-			
-			<?php /* Changes every other comment to a different class */	
-				if('graybox' == $oddcomment) {$oddcomment="";}
-				else { $oddcomment = "graybox"; }
-			?>
-	
-		<?php endforeach; /* end for each comment */ ?>	
-		</ol>
+
+			</li><!-- /.comment -->
+
+		<?php endforeach; ?>
+
+		</ol><!-- /.m-comments -->
 
 
 	<?php endif; ?>
-	
-</div>
 
 
-<!-- ============================================== -->
+</div><!-- /.o-box-comments -->
 
 
-<div class="nocomments" style="background-color: #eeedd5; padding: 16px;">
-	<p>Das war es mit elf Jahren Kommentaren auf <em>allesaussersport</em>. Ich habe im zunehmenden Maße keinen Sinn mehr gesehen, meine begrenzten Ressoucen auf eine wie auch immer geartete Verwaltung der Kommentare zu verwenden. Es ist an der Zeit einen Schlussstrich zu setzen. Die Kommentare sind und bleiben geschlossen.</p>
-	<p>Wer aber weiterhin Kommentare schreiben und/oder lesen möchte, für den hat Lesern <em>Sternburg</em> eine Alternative  aufgesetzt: <strong style="background-color: #cccdc5;"><a href="http://allesausseraas.de/">http://allesausseraas.de/</a></strong>. Macht dort keinen Unsinn.</p>
-</div>
 
